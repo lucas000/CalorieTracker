fff1545fd4511f0ee09546aa98500189746f5e0a
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2023-12-05 14:37:51 -0300

    Criando use case para filtrar dígitos e adicionando dependência no app module

--- core/src/main/java/com/plcoding/core/util/UiEvent.kt
+++ core/src/main/java/com/plcoding/core/util/UiEvent.kt
@@ -3,4 +3,5 @@ package com.plcoding.core.util
 sealed class UiEvent {
     data class Navigate(val route: String): UiEvent()
     object NavigateU : UiEvent()
+    data class ShowSnackBar(val message: UiText): UiEvent()
 }
